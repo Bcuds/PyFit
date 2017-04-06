472a118a3899afdebd12e343db5b4c45904d4bf6
Bcuds <ben.cutler14@gmail.com>
2017-04-05 21:34:10 -0500

    Pyfitting Version 1.0
    Includes:
    
    Linear data fitting

---
+++ bestfit.py
@@ -0,0 +1,154 @@
+# Software I wrote in Python to solve a Least Squares problem and do line fitting in Python.
+# Written by Benjamin B. Cutler in the Summer of 2016. Email bugs to ben.cutler14@gmail.com
+# Sample Matricies
+#A = [[1,2],[3,4]]
+#B = [[5,6],[7,8]]
+# [ 1 2 ] * [ 5 6 ] = [19 22]
+# [ 3 4 ]   [ 7 8 ]   [43 50]
+
+#import matplotlib.pyplot as plt
+
+def transpose(A):
+    outmatrix = []
+    for i in range(len(A[0])): # Generates a list of lists, which will be populated by elements
+        outmatrix.append([])
+        
+    for j in range(len(A[0])): # j makes up the Old cols, which become rows
+        for i in range(len(A)): #I makes up col rows, which become colss
+            outmatrix[j].append(A[i][j]) 
+        
+    return outmatrix
+def matrixproduct(A,B):
+#Computes a matrix product for a 'A' x 'a' and a 'B' x 'b' matrix presuming a = B and 
+# the output should be an 'A' by 'b' matrix
+    outmatrix = []
+    for i in range(len(B[0])): # Generates list of lists, which will be populated by elements
+        outmatrix.append([])
+
+    if len(A[0]) != len(B): # Makes sure the matricies have the same size.
+        print("Matricies must have equal values!")
+        return outmatrix # If they don't, this just returns a blank matrix, so that there isn't a value error, which breaks the user from the main function.
+
+    thing = 0  # Counter for which column. Acts like the 'i' in a for loop, but the rows need to be called too.   
+    for rowsofB in transpose(B):
+        for colsofA in A:
+            acc = 0
+            for i in range(len(A[0])):
+                acc = acc + colsofA[i]*rowsofB[i]
+            outmatrix[thing].append(acc)
+        thing += 1
+    return transpose(outmatrix)
+
+def rowreduce(A):
+    # Row reducing Algorithm
+    # Step 1: Make sure the matrix has a val in each pivot position
+    def pivotize(A):
+
+	    import random
+	    for i in range(1000):
+	        counter = -1
+	        for i in range(len(A)): # Loop through each pivot position
+	            if A[i][i] == 0:    # Check for a zero there
+	                chosenrow = random.randint(0,len(A)) # if there is one there, replace it with a random row.
+	                (A[i],A[chosenrow]) = (A[chosenrow],A[i])
+	                break
+	            else:
+	                counter = counter + 1
+	        if counter == i:
+	            break
+	        if i == 999 :
+	            print("Something has gone wrong")
+	            return None
+	    # Step 2 requires you to make each pivot equal to 1
+	    for i in range(len(A)):
+	        pivot = A[i][i]
+	        for j in range(len(A[i])):
+	            A[i][j] = A[i][j] / pivot
+	    return A
+	#If we are on the first column, we have to make sure it has a pivot in each available space
+    
+    # Step 3 involves taking each column, and making each value underneath it '0'
+    # We must first teach python row subtraction though.
+    def rowsubtract(row1,row2,n):
+        #row1 - n*row2 = outcol
+        #[1,2,3] - 5*[.2,.3,45] = [0.0, 0.5, -222]
+        # Essentially, you want row1 to be the row that ends up geting the zeroes value
+        outcol = []
+        for i in range(len(row1)):
+            outcol.append(row1[i]- n*row2[i])
+        return outcol
+    # Then we can write a loop which clears the first row. 
+    # In this nested loop, the loop var row represents the Row we are working on
+    # The variable 'col' is, then the column
+    # Then is goes through the row, and names each non pivot element 0
+    def solvesys(A):
+        # Computes the actual row reduction
+        for col in range(len(A[0]) -1):
+            A = pivotize(A)
+            for row in range(len(A)):
+                if col != row:
+                    A[row] = rowsubtract(A[row],A[col],A[row][col]/A[row][row])
+    solvesys(A)
+    return A
+
+def getdata():
+	#This function gets the data from the user. Inputted (hopefully copied) as a list
+	#It presumes you are solving for f(x) = y
+	print("Assuming the data input is a list of x values and a list of y values. ")
+	print("Enter a value like")
+	print("1,2,3,4")
+	data = []
+	var = ["x","y"]
+	for i in range(2):
+		print(" Please enter  of values of the dependent variable. This is the coordinate {0}".format(var[i]))
+		data.append(eval(input("")))
+		print(" Data points for  {0} has been entered".format(var[i]))
+	return data
+def printmatrix(A):
+    for line in A:
+        print(line)
+    print("")
+def formmatrix(indata): # Needs to be cleaned up a little bit
+    import math
+    # This takes the data, and puts it into a matrix, which will then be turned into
+    # A^T * A = A^T * b.
+    A = indata[0]
+    b = indata[1]
+    # The 'test case' is for a y = Var1*exp(ax) + Var2*exp(bx) Ideally I can find some way of making the user enter the specific function they want.
+    print("Next, we need to get the function you want to fit a line to")
+    print("For example, if you want to fit to y = C1 exp(-.02x) + C2 exp(-.07x),  then you have to input 'math.exp(-.02 * {0}),math.exp( -.07 * {0})'")
+    print("For a line, you would want '{0},1' , and this solves for 'm' and 'b' in y = mx + b")
+    print("essentially, and plus sign is a comma, and any coefficent has to be left blank. Make the dependent variable, x, {0}")
+    userfunction = input("Please enter the function you want to fit data to    ")
+    outmatrix = []
+    for num in A:
+        row = list(eval(userfunction.format(num)))
+        outmatrix.append(row)
+    # Same with 'newb' but this fix is a bit easier
+    newb = []
+    for elt in b:
+        newb = newb + [[elt]]
+    return outmatrix,newb
+
+def equate(A,b):
+    for i in range(len(A)):
+        A[i] = A[i] + b[i]
+    return(A)
+
+def bestfit( ):
+    # Data comes from the user, which I have to  make a bit better.
+    data = getdata()
+    # Gets the Matricies 'A' and 'b' In their ready to go states
+    A,b = formmatrix(data) 
+    #Compute Transpose(A)*A = Transpose(A)*b
+    left = matrixproduct(transpose(A),A)
+    right = matrixproduct(transpose(A),b)
+    #Turns thesystem into an augmented matrix
+    finalmatrix = equate(left,right)
+    #Rowreduces said matrix
+    finalmatrix = rowreduce(finalmatrix)
+    printmatrix(finalmatrix) 
+    #Plots user's input Data
+    #plt.scatter(data[0],data[1])    
+
+bestfit()
