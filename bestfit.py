753250c27cca60906ac077c5f1ab7610f900db10
Ben Cutler <ben.cutler14@gmail.com>
2017-05-18 00:22:48 -0400

    Add files via upload
    Added Lots of comments and cleaned up some code

--- bestfit.py
+++ bestfit.py
@@ -7,68 +7,91 @@
 # [ 3 4 ]   [ 7 8 ]   [43 50]
 
 #import matplotlib.pyplot as plt
+import math
 
 def transpose(A):
-    outmatrix = []
-    for i in range(len(A[0])): # Generates a list of lists, which will be populated by elements
+    #Pre Condition  : A Matrix, Pass by Reference
+    #Post Condition : Returns a copy of that matrix which has swapped rows and columns. Matrix A is uneffected by this operation
+    #Purpose        : To make a transpose of an arbitraty N x M matrix
+    #Sample I/O     : [1,2] <-> [1,3]
+    #               : [3,4] <-> [2,4]
+    
+    outmatrix = [] # Matrix that gets returned
+    for i in range(len(A[0])): # Generates a list of Rows, which will be populated by elements
         outmatrix.append([])
         
     for j in range(len(A[0])): # j makes up the Old cols, which become rows
         for i in range(len(A)): #I makes up col rows, which become colss
             outmatrix[j].append(A[i][j]) 
         
-    return outmatrix
+    return outmatrix 
+
 def matrixproduct(A,B):
-#Computes a matrix product for a 'A' x 'a' and a 'B' x 'b' matrix presuming a = B and 
-# the output should be an 'A' by 'b' matrix
+    # Pre Conditions : Two Matricies, 'A' and 'B', given Matrix A is 'Z' x 'z' and B is  'G' x 'g' matrix presuming g == z.
+    # Post Condition : Returns an Z x G matrix
+    # Purpose        : To compute the product between two matricies
+    # Sample I/O     :
+    #A = [[1,2],[3,4]]
+    #B = [[5,6],[7,8]]
+    # [ 1 2 ] * [ 5 6 ] = [19 22]
+    # [ 3 4 ]   [ 7 8 ]   [43 50]
+    
     outmatrix = []
     for i in range(len(B[0])): # Generates list of lists, which will be populated by elements
         outmatrix.append([])
 
     if len(A[0]) != len(B): # Makes sure the matricies have the same size.
         print("Matricies must have equal values!")
-        return outmatrix # If they don't, this just returns a blank matrix, so that there isn't a value error, which breaks the user from the main function.
-
-    thing = 0  # Counter for which column. Acts like the 'i' in a for loop, but the rows need to be called too.   
-    for rowsofB in transpose(B):
+        quit() # If they don't, this just returns a blank matrix, so that there isn't a value error, which breaks the user from the main function.
+    
+    # If we have valid input we compute the product
+    
+    for index,rowsofB in enumerate(transpose(B) ):
         for colsofA in A:
             acc = 0
             for i in range(len(A[0])):
-                acc = acc + colsofA[i]*rowsofB[i]
-            outmatrix[thing].append(acc)
-        thing += 1
+                acc += colsofA[i]*rowsofB[i]
+            outmatrix[index].append(acc)
+        
     return transpose(outmatrix)
 
-def rowreduce(A):
-    # Row reducing Algorithm
-    # Step 1: Make sure the matrix has a val in each pivot position
-    def pivotize(A):
-
-	    import random
-	    for i in range(1000):
-	        counter = -1
-	        for i in range(len(A)): # Loop through each pivot position
-	            if A[i][i] == 0:    # Check for a zero there
-	                chosenrow = random.randint(0,len(A)) # if there is one there, replace it with a random row.
-	                (A[i],A[chosenrow]) = (A[chosenrow],A[i])
-	                break
-	            else:
-	                counter = counter + 1
-	        if counter == i:
-	            break
-	        if i == 999 :
-	            print("Something has gone wrong")
-	            return None
-	    # Step 2 requires you to make each pivot equal to 1
-	    for i in range(len(A)):
-	        pivot = A[i][i]
-	        for j in range(len(A[i])):
-	            A[i][j] = A[i][j] / pivot
-	    return A
-	#If we are on the first column, we have to make sure it has a pivot in each available space
+def pivotize(A):
+    # Pre Condition   : A matrix 'A' which may not have a value in each pivot
+    # Post condition  : The matrix 'A' has the number '1' pivot in each position. Also returns 'A'
+    # Purpose         : To put a pivot in each position in the matrix.
+    # Sample I/O      : [0,7,4] -> [1,0,(1/3)]
+    #                 : [3,0,9] -> [0,1,(7/4)]
     
-    # Step 3 involves taking each column, and making each value underneath it '0'
-    # We must first teach python row subtraction though.
+    for i in range(1000): # This will try 1,000 times to pivotize the matrix.
+        counter = -1
+        for i in range(len(A)): # Loop through each pivot position
+            if A[i][i] == 0:    # Check for a zero there
+                chosenrow = random.randint(0,len(A)) # if there is one there, replace it with a random row.
+                (A[i],A[chosenrow]) = (A[chosenrow],A[i])
+            else:
+                counter = counter + 1
+        if counter == i:
+            break
+        if i == 999 :
+            print("Something has gone wrong in the pivotize function ")
+            printmatrix(A)
+            input("Press <Enter> to continue")            
+            quit()
+            
+    # Step 2 requires you to make each pivot equal to 1
+    for i in range(len(A)):
+        pivot = A[i][i]
+        for j in range(len(A[i])):
+            A[i][j] = A[i][j] / pivot # Sets each pivot to be equal to 1 by dividing the row by the pivot value
+    return A
+def rowreduce(A):
+    # Pre Condition : An N x N+1 matrix 'A'
+    # Post Cond     : Alters 'A' to be in Row Reduced Eschelon Form
+    # Purpose       : To take a matrix, and row reduce it to the RREF
+    # Sample I/O    : [1,-2,1] -> [1,0,-3]
+    #               : [-5,3,9] -> [0,1,-2]
+    
+    # We must first teach python row subtraction though. This function is defined here to keep it out of the scope of the other functions
     def rowsubtract(row1,row2,n):
         #row1 - n*row2 = outcol
         #[1,2,3] - 5*[.2,.3,45] = [0.0, 0.5, -222]
@@ -77,49 +100,66 @@ def rowreduce(A):
         for i in range(len(row1)):
             outcol.append(row1[i]- n*row2[i])
         return outcol
-    # Then we can write a loop which clears the first row. 
-    # In this nested loop, the loop var row represents the Row we are working on
-    # The variable 'col' is, then the column
-    # Then is goes through the row, and names each non pivot element 0
-    def solvesys(A):
-        # Computes the actual row reduction
-        for col in range(len(A[0]) -1):
-            A = pivotize(A)
-            for row in range(len(A)):
-                if col != row:
-                    A[row] = rowsubtract(A[row],A[col],A[row][col]/A[row][row])
-    solvesys(A)
+    
+    for col in range(len(A[0]) -1):
+        pivotize(A) 
+        for row in range(len(A)):
+            if col != row:
+                A[row] = rowsubtract(A[row],A[col],A[row][col]/A[row][row])
     return A
 
 def getdata():
-	#This function gets the data from the user. Inputted (hopefully copied) as a list
-	#It presumes you are solving for f(x) = y
-	print("Assuming the data input is a list of x values and a list of y values. ")
-	print("Enter a value like")
-	print("1,2,3,4")
-	data = []
-	var = ["x","y"]
-	for i in range(2):
-		print(" Please enter  of values of the dependent variable. This is the coordinate {0}".format(var[i]))
-		data.append(eval(input("")))
-		print(" Data points for  {0} has been entered".format(var[i]))
-	return data
+        #Pre Condition : Gets a comma seperated set of numbers from the user. // TODO : Make File Work
+        #Post condition: Stores it in a list, and returns a 2 x N list
+        #Purpose       : Gets data which will be used
+
+        
+        #This function gets the data from the user. Inputted (hopefully copied) as a list
+        #It presumes you are solving for f(x) = y
+        print("Assuming the data input is a list of x values and a list of y values. ")
+        print("Enter a value like")
+        print("1,2,3,4")
+        data = []
+        var = ["x","y"]
+        for i in range(2):
+            print(" Please enter  of values of the dependent variable. This is the coordinate {0}".format(var[i]))
+            data.append(eval(input("")))
+            print(" Data points for  {0} has been entered".format(var[i]))
+        return data
+
 def printmatrix(A):
+    # Primitive Method of printing a matrix
     for line in A:
         print(line)
     print("")
+
 def formmatrix(indata): # Needs to be cleaned up a little bit
-    import math
-    # This takes the data, and puts it into a matrix, which will then be turned into
-    # A^T * A = A^T * b.
+    # Pre Condition     : Data from the user.
+    # Post condition    : Returns a matrix, and a vector it is equal to
+    # Purpose           : This takes the data, and puts it into a matrix, which will then be turned into A^T * A = A^T * b.
+    # This returns the 'A' and 'b' in this equation. Row reducing the whole N x N+1 matrix yeilds the co-efficents of the least squares solution
+    
     A = indata[0]
     b = indata[1]
-    # The 'test case' is for a y = Var1*exp(ax) + Var2*exp(bx) Ideally I can find some way of making the user enter the specific function they want.
-    print("Next, we need to get the function you want to fit a line to")
-    print("For example, if you want to fit to y = C1 exp(-.02x) + C2 exp(-.07x),  then you have to input 'math.exp(-.02 * {0}),math.exp( -.07 * {0})'")
-    print("For a line, you would want '{0},1' , and this solves for 'm' and 'b' in y = mx + b")
-    print("essentially, and plus sign is a comma, and any coefficent has to be left blank. Make the dependent variable, x, {0}")
-    userfunction = input("Please enter the function you want to fit data to    ")
+    print("Select a function you would like to fit data to:")
+    print( "1. : A Line (y = mx +b)")
+    print( "2. : Custom Function")
+    usernumber = int (input("--") )
+    ##
+    #Get a function from the user to fit the data to
+    ##
+    if (usernumber == 1):
+        userfunction = "{0},1"
+        #coeffs = ['m','b']
+    elif (usernumber == 2):
+        # The 'test case' is for a y = Var1*exp(ax) + Var2*exp(bx) Ideally I can find some way of making the user enter the specific function they want.
+        print("For example, if you want to fit to y = C1 exp(-.02x) + C2 exp(-.07x),  then you have to input 'math.exp(-.02 * {0}),math.exp( -.07 * {0})'")
+        print("For a line, you would want '{0},1' , and this solves for 'm' and 'b' in y = mx + b")
+        print("essentially, and plus sign is a comma, and any coefficent has to be left blank. Make the dependent variable, x, {0}")
+        userfunction = input("Please enter the function you want to fit data to   \n ")
+    ##
+    # Make each row in the matrix that gets returned equal user's function -f- evaluated at 'x', where the '+' or '-' delimits elements of the row
+    ##
     outmatrix = []
     for num in A:
         row = list(eval(userfunction.format(num)))
@@ -131,9 +171,15 @@ def formmatrix(indata): # Needs to be cleaned up a little bit
     return outmatrix,newb
 
 def equate(A,b):
+    # Pre Condition     : A Matrix 'A' and a vector 'b'
+    # Post condition    : A new matrix which A concatinated with 'b' at the end of it
+    # Purpose           : To equate two matricies
+    # Sample I/O        : A = [1,2]  b = [7]  ->  return [1,2,7]
+    #                   :     [5,6]      [8]  ->         [5,6,8]
+    outmatrix = []
     for i in range(len(A)):
-        A[i] = A[i] + b[i]
-    return(A)
+        outmatrix.append( A[i] + b[i])
+    return outmatrix
 
 def bestfit( ):
     # Data comes from the user, which I have to  make a bit better.
@@ -146,8 +192,9 @@ def bestfit( ):
     #Turns thesystem into an augmented matrix
     finalmatrix = equate(left,right)
     #Rowreduces said matrix
-    finalmatrix = rowreduce(finalmatrix)
+    rowreduce(finalmatrix)
     printmatrix(finalmatrix) 
+    test = input()
     #Plots user's input Data
     #plt.scatter(data[0],data[1])    
 
